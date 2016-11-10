044b96d4528b8adb988c8bdec703aaee20dcca58
yijaejun <yijaejun81@gmail.com>
2016-11-10 13:46:42 +0900

    this is basic set for API server

---
+++ app/app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var routes = require('./routes/index');
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// routing
+app.use('/v1', routes);
+
+module.exports = app;
---
+++ app/routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/list', function(req, res, next) {
+  res.json({
+    'success' : true
+  });
+});
+
+module.exports = router;
